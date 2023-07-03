0396efaeeeb36aaf0a3344f8eb8c0c6ca5d02878
edd-ie <ogendoeddie@gmail.com>
2023-07-03 13:25:31 +0300

    Body margins fix

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import './Dashboard.css'
 
 export default function Dashboard() {
 
 
     return (
-        <div>
-            Home
+        <div id='eDashboard'>
+            <div id='eSidebar'></div>
+            <div id='eHeader'></div>
+            <div id='eContent'></div>
         </div>
     );
 }
