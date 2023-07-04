8362cd517b926dd8d744524ab0079e7c18ceac07
Naulikha <nbiketia@gmail.com>
2023-07-04 14:19:42 +0300

    Add Scrap Code

--- src/components/Mobile.jsx
+++ src/components/Mobile.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
-
+import "./styles/mobile.css"
 export default function Mobile() {
 
 
 
-    return (
-        <div>
-            Simu yangu 
-        </div>
-    );
-}
+    const Cards = ({ transactions, balance, category }) => {
+        return (
+          <div className="aMobile">
+            <h2>Client Transactions</h2>
+            <ul>
+              {transactions.map((transaction, index) => (
+                <li key={index}>{transaction}</li>
+              ))}
+            </ul>
+            <div className="aMobile-info">
+              <div>
+                <strong>Balance:</strong> {balance}
+              </div>
+              <div>
+                <strong>Category:</strong> {category}
+              </div>
+            </div>
+          </div>
+        );
+      };
+      }
