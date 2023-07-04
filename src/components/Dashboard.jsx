379317285bca6c6216cf7b77f7e835a65f5eb37b
edd-ie <ogendoeddie@gmail.com>
2023-07-04 12:05:48 +0300

    Creation of pie chart for data visualization

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import './Dashboard.css'
+import DashPie from './Dashpie';
 
 export default function Dashboard() {
 
 
     return (
         <div id='eDashboard'>
-            {/* Dashboard header */}
+            {/* Dashboard header  
+             https://www.youtube.com/watch?v=StGOX_gZeLU
+             https://nivo.rocks/pie/
+             */}
             <div id='eHeader'>
                 <div id='eLogo'></div>
                 <div id='eRoutes'></div>
@@ -16,7 +20,10 @@ export default function Dashboard() {
             {/* Dashboard main content */}
             <div id='eMain'>
                 <div id='eSidebar'></div>
-                <div id='eContent'></div>
+
+                <div id='eContent'>
+                    <DashPie/>
+                </div>
             </div>
         </div>
     );
